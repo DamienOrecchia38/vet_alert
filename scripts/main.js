0e489cd97780bc86bda26db840c096fbd43b2637
DamienOrecchia38 <156789771+DamienOrecchia38@users.noreply.github.com>
2024-01-23 16:21:49 +0100

    Final changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     { photo: 'images/madame_plume.jpg', title: 'Débat philosphique avec Madame Plume, le perroquet philosophe', date: '2025-05-01', description: 'Proposer un débat intense sur la signification profonde de la vie des oiseaux, des discussions existentielles et des réflexions plumeuses.' },
     { photo: 'images/poisson_rouge_peur.jpg', title: 'Consultation de psychothérapie pour Oscar le poisson rouge', date: '2025-06-02', description: 'Oscar souhaite discuter de ses angoissess existentielles liées à la taille de son bocal. Se préparer à plonger dans les profondeurs de l\'esprit du poisson rouge.' },
     { photo: 'images/meditation_cat.jpg', title: 'Séance de méditation avec Monsieur Miaou', date: '2025-07-08', description: 'La patient souhaite atteindre un niveau supérieur de zénitude féline et aimerait ronronner en harmonie avec l\'univers.' },
-    { photo: 'images/bulldog.jpg', title: 'Exposition d\'art canin avec Pablo le bulldog narcissique', date: '2025-09-14', description: 'Pablo le bulldog expose ses dernières œuvres d\'art abstraites, dont sa célèbre toile \'Le Chien Qui Rêve\'. Admirer ses talents artistiques pour regonfler son égo.".' },
+    { photo: 'images/bulldog.jpg', title: 'Exposition d\'art canin avec Pablo le bulldog narcissique', date: '2025-09-14', description: 'Pablo le bulldog expose ses dernières œuvres d\'art abstraites, dont sa célèbre toile \'Le Chien Qui Rêve\'. Admirer ses talents artistiques pour regonfler son égo.' },
     { photo: 'images/lapin.jpg', title: 'Réunion Secrète des Lapins Philanthropes', date: '2025-09-14', description: 'Les lapins philanthropes se réunissent pour discuter de leur mission secrète visant à distribuer des carottes à tous les animaux nécessiteux. Penser à apporter des légumes frais.' },
     { photo: 'images/panda.jpg', title: 'Thérapie de groupe pour animaux insomniaques', date: '2025-09-14', description: 'Diffuser des ASMR de ronflements apaisants.' },
   ];
@@ -29,18 +29,18 @@ function createNotificationElement(notification) {
   notificationCard.classList.add('col-md-6', 'col-lg-3', 'mb-4');
 
   notificationCard.innerHTML = `
-  <div class="card h-100 bg-light shadow">
-  <img src="${notification.photo}" class="card-img-top" alt="${notification.title}">
-  <div class="card-body">
-    <div class="logo-with-date">
-      <img class="logo-alert" src="images/alerte.png" alt="logo alerte rouge">
-      <h6 class="card-title">${notification.title}</h6>
+    <div class="card h-100 bg-light shadow">
+      <img src="${notification.photo}" class="card-img-top" alt="${notification.title}">
+      <div class="card-body">
+        <div class="logo-with-title">
+          <img class="logo-alert" src="images/alerte.png" alt="logo alerte rouge">
+          <h6 class="card-title">${notification.title}</h6>
+        </div>
+        <HR>
+        <p class="card-text">${notification.description}</p>
+        <p class="card-text"><small class="text-muted">${formatDate(notification.date)}</small></p>
+      </div>
     </div>
-    <HR>
-    <p class="card-text">${notification.description}</p>
-    <p class="card-text"><small class="text-muted">${formatDate(notification.date)}</small></p>
-  </div>
-</div>
   `;
 
   return notificationCard;
